fc3df3cdfb82b4d4719a7ad9188a57b7e61e4e04
Durian <dorian@luyet.ch>
2026-04-30 15:32:50 +0200

    fix: update version in package-lock.json and modify footer links and copyright information feat: add Dockerfile for containerization

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -35,11 +35,11 @@
 <footer>
     <div id="links">
         <h3>{m.liens()}</h3>
-        <a href="https://github.com/Dexerix/OhmCalc-sv"><img src="/github.svg" alt="Github"></a>
+        <a href="https://github.com/DurianTheWAV/OhmCalc-sv"><img src="/github.svg" alt="Github"></a>
         <a href="https://ko-fi.com/L3L11A3Z1N" target="_blank"><img height="36" style="border:0px;height:36px;" src="https://storage.ko-fi.com/cdn/kofi3.png?v=6" alt="Buy Me a Coffee at ko-fi.com"></a>
     </div>
     <div id="copyright">
-        <p>Made with ❤️ by <a href="https://github.com/Dexerix/">Dexerix</a> in Switzerland</p>
-        <p>© 2025 OhmCalc-sv</p>
+        <p>Made with ❤️ by <a href="https://github.com/DurianTheWAV/">DurianTheWAV</a> in Switzerland</p>
+        <p>© 2025-2026 OhmCalc-sv</p>
     </div>
 </footer>
