8b8eb80915b2169e84d14aec31d3b6983455df2e
Durian <dorian@luyet.ch>
2026-06-11 14:11:40 +0200

    feat: add Svelte adapter-static and update base locale to German

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
     import type { PageData } from './$types';
     let { children } = $props();
     import Footer from './Footer.svelte';
-    import '../styles/global.css'
+    import '../styles/global.css';
 </script>
 
 {@render children()}
